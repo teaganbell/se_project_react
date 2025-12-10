103766d0addacd652f1864ceb00dfdcd68d2eec0
TeaganBell <teagan.bell96@gmail.com>
2025-12-09 17:56:06 -0700

    Build to docs folder for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/se_project_react/",
+  build: {
+    outDir: "docs",
+  },
   // add the server object
   server: {
     port: 3000,
